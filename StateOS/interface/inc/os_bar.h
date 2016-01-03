ae07cb7e76926e8575392c860469c01a1beaa447
Rajmund Szymanski <rajszym@o2.pl>
2016-01-03 18:32:11 +0100

    os.bar.h

--- StateOS/interface/inc/os_bar.h
+++ StateOS/interface/inc/os_bar.h
@@ -2,7 +2,7 @@
 
     @file    State Machine OS: os_bar.h
     @author  Rajmund Szymanski
-    @date    23.12.2015
+    @date    03.01.2016
     @brief   This file contains definitions for StateOS.
 
  ******************************************************************************
@@ -34,45 +34,131 @@
 extern "C" {
 #endif
 
-/* -------------------------------------------------------------------------- */
-
-// deklaracja bariery 'bar'
-
-#define OS_BAR( bar, limit )                       \
+/**********************************************************************************************************************
+ *                                                                                                                    *
+ * Name              : OS_BAR                                                                                         *
+ *                                                                                                                    *
+ * Description       : define a barrier object                                                                        *
+ *                                                                                                                    *
+ * Parameters                                                                                                         *
+ *   bar             : name of a pointer to barrier object                                                            *
+ *   limit           : maximum count of barrier                                                                       *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
+#define     OS_BAR( bar, limit )                   \
                bar_t bar##__bar = _BAR_INIT(limit); \
                bar_id bar = & bar##__bar
 
+/**********************************************************************************************************************
+ *                                                                                                                    *
+ * Name              : static_BAR                                                                                     *
+ *                                                                                                                    *
+ * Description       : define a static barrier object                                                                 *
+ *                                                                                                                    *
+ * Parameters                                                                                                         *
+ *   bar             : name of a pointer to barrier object                                                            *
+ *   limit           : maximum count of barrier                                                                       *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
 #define static_BAR( bar, limit )                   \
         static bar_t bar##__bar = _BAR_INIT(limit); \
         static bar_id bar = & bar##__bar
 
-/* -------------------------------------------------------------------------- */
-
-// utworzenie obiektu typu bariera
-// zwraca adres utworzonego obiektu, lub 0
+/**********************************************************************************************************************
+ *                                                                                                                    *
+ * Name              : bar_create                                                                                     *
+ *                                                                                                                    *
+ * Description       : create a new barrier object                                                                    *
+ *                                                                                                                    *
+ * Parameters                                                                                                         *
+ *   limit           : maximum count of barrier                                                                       *
+ *                                                                                                                    *
+ * Return            : pointer to barrier object (barrier successfully created)                                       *
+ *   0               : barrier not created (not enough free memory)                                                   *
+ *                                                                                                                    *
+ * Notes             : function called from thread mode                                                               *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
               bar_id   bar_create( unsigned limit );
 
-// reset obiektu 'bar'
-// wszystkie procesy oczekujace zostaja wybudzone
-// zostaje do nich wyslany komunikat E_STOPPED
+/**********************************************************************************************************************
+ *                                                                                                                    *
+ * Name              : bar_kill                                                                                       *
+ *                                                                                                                    *
+ * Description       : reset barrier object and wake up all waiting tasks with 'E_STOPPED' event value                *
+ *                                                                                                                    *
+ * Parameters                                                                                                         *
+ *   bar             : pointer to barrier object                                                                      *
+ *                                                                                                                    *
+ * Return            : none                                                                                           *
+ *                                                                                                                    *
+ * Notes             : function called from thread mode                                                               *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
               void     bar_kill( bar_id bar );
 
-// zawieszenie wykonywania aktualnego procesu do czasu 'time'
-// lub do wybudzenia przez obiekt 'bar'
-// zwraca E_SUCCESS, E_STOPPED lub E_TIMEOUT
+/**********************************************************************************************************************
+ *                                                                                                                    *
+ * Name              : bar_waitUntil                                                                                  *
+ *                                                                                                                    *
+ * Description       : wait for barrier object until given timepoint                                                  *
+ *                                                                                                                    *
+ * Parameters                                                                                                         *
+ *   bar             : pointer to barrier object                                                                      *
+ *   time            : timepoint value                                                                                *
+ *                                                                                                                    *
+ * Return                                                                                                             *
+ *   E_SUCCESS       : barrier object was successfully released                                                       *
+ *   E_STOPPED       : barrier object has been killed                                                                 *
+ *   E_TIMEOUT       : barrier object was not released before the specified timeout expired                           *
+ *   'another'       : task was resumed with 'another' event                                                          *
+ *                                                                                                                    *
+ * Notes             : function called from thread mode                                                               *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
               unsigned bar_waitUntil( bar_id bar, unsigned time );
 
-// zawieszenie wykonywania aktualnego procesu na czas 'delay'
-// lub do wybudzenia przez obiekt 'bar'
-// zwraca E_SUCCESS, E_STOPPED lub E_TIMEOUT
+/**********************************************************************************************************************
+ *                                                                                                                    *
+ * Name              : bar_waitFor                                                                                    *
+ *                                                                                                                    *
+ * Description       : wait for barrier object for given duration of time                                             *
+ *                                                                                                                    *
+ * Parameters                                                                                                         *
+ *   bar             : pointer to barrier object                                                                      *
+ *   delay           : duration of time (maximum number of ticks to wait for release of the barrier object)           *
+ *                     IMMEDIATE: don't wait if the barrier object can't be released                                  *
+ *                     INFINITE:  wait indefinitly until the barrier object have been released                        *
+ *                                                                                                                    *
+ * Return                                                                                                             *
+ *   E_SUCCESS       : barrier object was successfully released                                                       *
+ *   E_STOPPED       : barrier object has been killed                                                                 *
+ *   E_TIMEOUT       : barrier object was not released before the specified timeout expired                           *
+ *   'another'       : task was resumed with 'another' event value                                                    *
+ *                                                                                                                    *
+ * Notes             : function called from thread mode                                                               *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
               unsigned bar_waitFor( bar_id bar, unsigned delay );
 
-// zawieszenie wykonywania aktualnego procesu
-// do czasu wybudzenia przez obiekt 'bar'
-// zwraca E_SUCCESS lub E_STOPPED
+/**********************************************************************************************************************
+ *                                                                                                                    *
+ * Name              : bar_wait                                                                                       *
+ *                                                                                                                    *
+ * Description       : wait for barrier object                                                                        *
+ *                                                                                                                    *
+ * Parameters                                                                                                         *
+ *   bar             : pointer to barrier object                                                                      *
+ *                                                                                                                    *
+ * Return                                                                                                             *
+ *   E_SUCCESS       : barrier object was successfully released                                                       *
+ *   E_STOPPED       : barrier object has been killed                                                                 *
+ *   'another'       : task was resumed with 'another' event value                                                    *
+ *                                                                                                                    *
+ * Notes             : function called from thread mode                                                               *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
 static inline unsigned bar_wait( bar_id bar ) { return bar_waitFor(bar, INFINITE); }
-
-/* -------------------------------------------------------------------------- */
 
 #ifdef __cplusplus
 }
