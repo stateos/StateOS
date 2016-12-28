3f687128fab985222f216e764923275a426c03e3
Rajmund Szymanski <rajszym@o2.pl>
2016-12-28 22:58:34 +0100

    update stm32f4_io

--- device/STM32F4/stm32f4_io.h
+++ device/STM32F4/stm32f4_io.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  * @file    stm32f4_io.h
  * @author  Rajmund Szymanski
- * @date    24.10.2016
+ * @date    28.12.2016
  * @brief   This file contains macro definitions for the STM32F4XX GPIO ports.
  ******************************************************************************/
 
@@ -27,117 +27,122 @@ struct __gpio_config
 	unsigned af:     4;
 };
 */
-#define GPIO_OUT_Pos      (0)
-#define GPIO_MODE_Pos     (1)
-#define GPIO_OTYPE_Pos    (3)
-#define GPIO_OSPEED_Pos   (4)
-#define GPIO_PUPD_Pos     (6)
-#define GPIO_AF_Pos       (8)
+#define GPIO_OUT_Pos      (0U)
+#define GPIO_OUT_Msk      (0x1UL << GPIO_OUT_Pos)
+#define GPIO_OUT(cfg)     _FLD2VAL(GPIO_OUT, cfg)
 
-#define GPIO_OUT(cfg)     (0x1u&((cfg)>>GPIO_OUT_Pos))
+#define GPIO_Reset        _VAL2FLD(GPIO_OUT, 0) /* reset output pin */
+#define GPIO_Set          _VAL2FLD(GPIO_OUT, 1)   /* set output pin */
 
-#define GPIO_Reset        (0x0u<<GPIO_OUT_Pos)  /* reset output pin */
-#define GPIO_Set          (0x1u<<GPIO_OUT_Pos)  /* set output pin */
+#define GPIO_MODE_Pos     (1U)
+#define GPIO_MODE_Msk     (0x3UL << GPIO_MODE_Pos)
+#define GPIO_MODE(cfg)    _FLD2VAL(GPIO_MODE, cfg)
 
-#define GPIO_MODE(cfg)    (0x3u&((cfg)>>GPIO_MODE_Pos))
+#define GPIO_Input        _VAL2FLD(GPIO_MODE, 0)
+#define GPIO_Output       _VAL2FLD(GPIO_MODE, 1)
+#define GPIO_Alternate    _VAL2FLD(GPIO_MODE, 2)
+#define GPIO_Analog       _VAL2FLD(GPIO_MODE, 3)
 
-#define GPIO_Input        (0x0u<<GPIO_MODE_Pos)
-#define GPIO_Output       (0x1u<<GPIO_MODE_Pos)
-#define GPIO_Alternate    (0x2u<<GPIO_MODE_Pos)
-#define GPIO_Analog       (0x3u<<GPIO_MODE_Pos)
+#define GPIO_OTYPE_Pos    (3U)
+#define GPIO_OTYPE_Msk    (0x1UL << GPIO_OTYPE_Pos)
+#define GPIO_OTYPE(cfg)   _FLD2VAL(GPIO_OTYPE, cfg)
 
-#define GPIO_OTYPE(cfg)   (0x1u&((cfg)>>GPIO_OTYPE_Pos))
+#define GPIO_PushPull     _VAL2FLD(GPIO_OTYPE, 0)
+#define GPIO_OpenDrain    _VAL2FLD(GPIO_OTYPE, 1)
 
-#define GPIO_PushPull     (0x0u<<GPIO_OTYPE_Pos)
-#define GPIO_OpenDrain    (0x1u<<GPIO_OTYPE_Pos)
+#define GPIO_OSPEED_Pos   (4U)
+#define GPIO_OSPEED_Msk   (0x3UL << GPIO_OSPEED_Pos)
+#define GPIO_OSPEED(cfg)  _FLD2VAL(GPIO_OSPEED, cfg)
 
-#define GPIO_OSPEED(cfg)  (0x3u&((cfg)>>GPIO_OSPEED_Pos))
+#define GPIO_2MHz         _VAL2FLD(GPIO_OSPEED, 0)
+#define GPIO_25MHz        _VAL2FLD(GPIO_OSPEED, 1)
+#define GPIO_50MHz        _VAL2FLD(GPIO_OSPEED, 2)
+#define GPIO_100MHz       _VAL2FLD(GPIO_OSPEED, 3)
 
-#define GPIO_2MHz         (0x0u<<GPIO_OSPEED_Pos)
-#define GPIO_25MHz        (0x1u<<GPIO_OSPEED_Pos)
-#define GPIO_50MHz        (0x2u<<GPIO_OSPEED_Pos)
-#define GPIO_100MHz       (0x3u<<GPIO_OSPEED_Pos)
+#define GPIO_PUPD_Pos     (6U)
+#define GPIO_PUPD_Msk     (0x3UL << GPIO_PUPD_Pos)
+#define GPIO_PUPD(cfg)    _FLD2VAL(GPIO_PUPD, cfg)
 
-#define GPIO_PUPD(cfg)    (0x3u&((cfg)>>GPIO_PUPD_Pos))
+#define GPIO_NoPull       _VAL2FLD(GPIO_PUPD, 0)
+#define GPIO_PullUp       _VAL2FLD(GPIO_PUPD, 1)
+#define GPIO_PullDown     _VAL2FLD(GPIO_PUPD, 2)
 
-#define GPIO_NoPull       (0x0u<<GPIO_PUPD_Pos)
-#define GPIO_PullUp       (0x1u<<GPIO_PUPD_Pos)
-#define GPIO_PullDown     (0x2u<<GPIO_PUPD_Pos)
+#define GPIO_AF_Pos       (8U)
+#define GPIO_AF_Msk       (0xFUL << GPIO_AF_Pos)
+#define GPIO_AF(cfg)      _FLD2VAL(GPIO_AF, cfg)
 
-#define GPIO_AF(cfg)      (0xFu&((cfg)>>GPIO_AF_Pos))
-
-#define GPIO_AF_RTC_50Hz  (0x0u<<GPIO_AF_Pos)  /* RTC_50Hz Alternate Function mapping */
-#define GPIO_AF_MCO       (0x0u<<GPIO_AF_Pos)  /* MCO (MCO1 and MCO2) Alternate Function mapping */
-#define GPIO_AF_TAMPER    (0x0u<<GPIO_AF_Pos)  /* TAMPER (TAMPER_1 and TAMPER_2) Alternate Function mapping */
-#define GPIO_AF_SWJ       (0x0u<<GPIO_AF_Pos)  /* SWJ (SWD and JTAG) Alternate Function mapping */
-#define GPIO_AF_TRACE     (0x0u<<GPIO_AF_Pos)  /* TRACE Alternate Function mapping */
-#define GPIO_AF_TIM1      (0x1u<<GPIO_AF_Pos)  /* TIM1 Alternate Function mapping */
-#define GPIO_AF_TIM2      (0x1u<<GPIO_AF_Pos)  /* TIM2 Alternate Function mapping */
-#define GPIO_AF_TIM3      (0x2u<<GPIO_AF_Pos)  /* TIM3 Alternate Function mapping */
-#define GPIO_AF_TIM4      (0x2u<<GPIO_AF_Pos)  /* TIM4 Alternate Function mapping */
-#define GPIO_AF_TIM5      (0x2u<<GPIO_AF_Pos)  /* TIM5 Alternate Function mapping */
-#define GPIO_AF_TIM8      (0x3u<<GPIO_AF_Pos)  /* TIM8 Alternate Function mapping */
-#define GPIO_AF_TIM9      (0x3u<<GPIO_AF_Pos)  /* TIM9 Alternate Function mapping */
-#define GPIO_AF_TIM10     (0x3u<<GPIO_AF_Pos)  /* TIM10 Alternate Function mapping */
-#define GPIO_AF_TIM11     (0x3u<<GPIO_AF_Pos)  /* TIM11 Alternate Function mapping */
-#define GPIO_AF_I2C1      (0x4u<<GPIO_AF_Pos)  /* I2C1 Alternate Function mapping */
-#define GPIO_AF_I2C2      (0x4u<<GPIO_AF_Pos)  /* I2C2 Alternate Function mapping */
-#define GPIO_AF_I2C3      (0x4u<<GPIO_AF_Pos)  /* I2C3 Alternate Function mapping */
-#define GPIO_AF_SPI1      (0x5u<<GPIO_AF_Pos)  /* SPI1 Alternate Function mapping */
-#define GPIO_AF_SPI2      (0x5u<<GPIO_AF_Pos)  /* SPI2/I2S2 Alternate Function mapping */
-#define GPIO_AF_SPI4      (0x5u<<GPIO_AF_Pos)  /* SPI4 Alternate Function mapping */
-#define GPIO_AF_SPI5      (0x5u<<GPIO_AF_Pos)  /* SPI5 Alternate Function mapping */
-#define GPIO_AF_SPI6      (0x5u<<GPIO_AF_Pos)  /* SPI6 Alternate Function mapping */
-#define GPIO_AF_SPI3      (0x6u<<GPIO_AF_Pos)  /* SPI3/I2S3 Alternate Function mapping */
-#define GPIO_AF_SAI1      (0x6u<<GPIO_AF_Pos)  /* SAI1 Alternate Function mapping */
-#define GPIO_AF_USART1    (0x7u<<GPIO_AF_Pos)  /* USART1 Alternate Function mapping */
-#define GPIO_AF_USART2    (0x7u<<GPIO_AF_Pos)  /* USART2 Alternate Function mapping */
-#define GPIO_AF_USART3    (0x7u<<GPIO_AF_Pos)  /* USART3 Alternate Function mapping */
-#define GPIO_AF_I2S3ext   (0x7u<<GPIO_AF_Pos)  /* I2S3ext Alternate Function mapping */
-#define GPIO_AF_UART4     (0x8u<<GPIO_AF_Pos)  /* UART4 Alternate Function mapping */
-#define GPIO_AF_UART5     (0x8u<<GPIO_AF_Pos)  /* UART5 Alternate Function mapping */
-#define GPIO_AF_USART6    (0x8u<<GPIO_AF_Pos)  /* USART6 Alternate Function mapping */
-#define GPIO_AF_UART7     (0x8u<<GPIO_AF_Pos)  /* UART7 Alternate Function mapping */
-#define GPIO_AF_UART8     (0x8u<<GPIO_AF_Pos)  /* UART8 Alternate Function mapping */
-#define GPIO_AF_CAN1      (0x9u<<GPIO_AF_Pos)  /* CAN1 Alternate Function mapping */
-#define GPIO_AF_CAN2      (0x9u<<GPIO_AF_Pos)  /* CAN2 Alternate Function mapping */
-#define GPIO_AF_TIM12     (0x9u<<GPIO_AF_Pos)  /* TIM12 Alternate Function mapping */
-#define GPIO_AF_TIM13     (0x9u<<GPIO_AF_Pos)  /* TIM13 Alternate Function mapping */
-#define GPIO_AF_TIM14     (0x9u<<GPIO_AF_Pos)  /* TIM14 Alternate Function mapping */
-#define GPIO_AF9_I2C2     (0x9u<<GPIO_AF_Pos)  /* I2C2 Alternate Function mapping (Only for STM32F401xx Devices) */
-#define GPIO_AF9_I2C3     (0x9u<<GPIO_AF_Pos)  /* I2C3 Alternate Function mapping (Only for STM32F401xx Devices) */
-#define GPIO_AF_OTG_FS    (0xAu<<GPIO_AF_Pos)  /* OTG_FS Alternate Function mapping */
-#define GPIO_AF_OTG_HS    (0xAu<<GPIO_AF_Pos)  /* OTG_HS Alternate Function mapping */
-#define GPIO_AF_ETH       (0xBu<<GPIO_AF_Pos)  /* ETHERNET Alternate Function mapping */
-#define GPIO_AF_FSMC      (0xCu<<GPIO_AF_Pos)  /* FSMC Alternate Function mapping */
-#define GPIO_AF_FMC       (0xCu<<GPIO_AF_Pos)  /* FMC Alternate Function mapping */
-#define GPIO_AF_OTG_HS_FS (0xCu<<GPIO_AF_Pos)  /* OTG HS configured in FS, Alternate Function mapping */
-#define GPIO_AF_SDIO      (0xCu<<GPIO_AF_Pos)  /* SDIO Alternate Function mapping */
-#define GPIO_AF_DCMI      (0xDu<<GPIO_AF_Pos)  /* DCMI Alternate Function mapping */
-#define GPIO_AF_LTDC      (0xEu<<GPIO_AF_Pos)  /* LCD-TFT Alternate Function mapping */
-#define GPIO_AF_EVENTOUT  (0xFu<<GPIO_AF_Pos)  /* EVENTOUT Alternate Function mapping */
+#define GPIO_AF_RTC_50Hz  _VAL2FLD(GPIO_AF, 0)  /* RTC_50Hz Alternate Function mapping */
+#define GPIO_AF_MCO       _VAL2FLD(GPIO_AF, 0)  /* MCO (MCO1 and MCO2) Alternate Function mapping */
+#define GPIO_AF_TAMPER    _VAL2FLD(GPIO_AF, 0)  /* TAMPER (TAMPER_1 and TAMPER_2) Alternate Function mapping */
+#define GPIO_AF_SWJ       _VAL2FLD(GPIO_AF, 0)  /* SWJ (SWD and JTAG) Alternate Function mapping */
+#define GPIO_AF_TRACE     _VAL2FLD(GPIO_AF, 0)  /* TRACE Alternate Function mapping */
+#define GPIO_AF_TIM1      _VAL2FLD(GPIO_AF, 1)  /* TIM1 Alternate Function mapping */
+#define GPIO_AF_TIM2      _VAL2FLD(GPIO_AF, 1)  /* TIM2 Alternate Function mapping */
+#define GPIO_AF_TIM3      _VAL2FLD(GPIO_AF, 2)  /* TIM3 Alternate Function mapping */
+#define GPIO_AF_TIM4      _VAL2FLD(GPIO_AF, 2)  /* TIM4 Alternate Function mapping */
+#define GPIO_AF_TIM5      _VAL2FLD(GPIO_AF, 2)  /* TIM5 Alternate Function mapping */
+#define GPIO_AF_TIM8      _VAL2FLD(GPIO_AF, 3)  /* TIM8 Alternate Function mapping */
+#define GPIO_AF_TIM9      _VAL2FLD(GPIO_AF, 3)  /* TIM9 Alternate Function mapping */
+#define GPIO_AF_TIM10     _VAL2FLD(GPIO_AF, 3)  /* TIM10 Alternate Function mapping */
+#define GPIO_AF_TIM11     _VAL2FLD(GPIO_AF, 3)  /* TIM11 Alternate Function mapping */
+#define GPIO_AF_I2C1      _VAL2FLD(GPIO_AF, 4)  /* I2C1 Alternate Function mapping */
+#define GPIO_AF_I2C2      _VAL2FLD(GPIO_AF, 4)  /* I2C2 Alternate Function mapping */
+#define GPIO_AF_I2C3      _VAL2FLD(GPIO_AF, 4)  /* I2C3 Alternate Function mapping */
+#define GPIO_AF_SPI1      _VAL2FLD(GPIO_AF, 5)  /* SPI1 Alternate Function mapping */
+#define GPIO_AF_SPI2      _VAL2FLD(GPIO_AF, 5)  /* SPI2/I2S2 Alternate Function mapping */
+#define GPIO_AF_SPI4      _VAL2FLD(GPIO_AF, 5)  /* SPI4 Alternate Function mapping */
+#define GPIO_AF_SPI5      _VAL2FLD(GPIO_AF, 5)  /* SPI5 Alternate Function mapping */
+#define GPIO_AF_SPI6      _VAL2FLD(GPIO_AF, 5)  /* SPI6 Alternate Function mapping */
+#define GPIO_AF_SPI3      _VAL2FLD(GPIO_AF, 6)  /* SPI3/I2S3 Alternate Function mapping */
+#define GPIO_AF_SAI1      _VAL2FLD(GPIO_AF, 6)  /* SAI1 Alternate Function mapping */
+#define GPIO_AF_USART1    _VAL2FLD(GPIO_AF, 7)  /* USART1 Alternate Function mapping */
+#define GPIO_AF_USART2    _VAL2FLD(GPIO_AF, 7)  /* USART2 Alternate Function mapping */
+#define GPIO_AF_USART3    _VAL2FLD(GPIO_AF, 7)  /* USART3 Alternate Function mapping */
+#define GPIO_AF_I2S3ext   _VAL2FLD(GPIO_AF, 7)  /* I2S3ext Alternate Function mapping */
+#define GPIO_AF_UART4     _VAL2FLD(GPIO_AF, 8)  /* UART4 Alternate Function mapping */
+#define GPIO_AF_UART5     _VAL2FLD(GPIO_AF, 8)  /* UART5 Alternate Function mapping */
+#define GPIO_AF_USART6    _VAL2FLD(GPIO_AF, 8)  /* USART6 Alternate Function mapping */
+#define GPIO_AF_UART7     _VAL2FLD(GPIO_AF, 8)  /* UART7 Alternate Function mapping */
+#define GPIO_AF_UART8     _VAL2FLD(GPIO_AF, 8)  /* UART8 Alternate Function mapping */
+#define GPIO_AF_CAN1      _VAL2FLD(GPIO_AF, 9)  /* CAN1 Alternate Function mapping */
+#define GPIO_AF_CAN2      _VAL2FLD(GPIO_AF, 9)  /* CAN2 Alternate Function mapping */
+#define GPIO_AF_TIM12     _VAL2FLD(GPIO_AF, 9)  /* TIM12 Alternate Function mapping */
+#define GPIO_AF_TIM13     _VAL2FLD(GPIO_AF, 9)  /* TIM13 Alternate Function mapping */
+#define GPIO_AF_TIM14     _VAL2FLD(GPIO_AF, 9)  /* TIM14 Alternate Function mapping */
+#define GPIO_AF9_I2C2     _VAL2FLD(GPIO_AF, 9)  /* I2C2 Alternate Function mapping (Only for STM32F401xx Devices) */
+#define GPIO_AF9_I2C3     _VAL2FLD(GPIO_AF, 9)  /* I2C3 Alternate Function mapping (Only for STM32F401xx Devices) */
+#define GPIO_AF_OTG_FS    _VAL2FLD(GPIO_AF,10)  /* OTG_FS Alternate Function mapping */
+#define GPIO_AF_OTG_HS    _VAL2FLD(GPIO_AF,10)  /* OTG_HS Alternate Function mapping */
+#define GPIO_AF_ETH       _VAL2FLD(GPIO_AF,11)  /* ETHERNET Alternate Function mapping */
+#define GPIO_AF_FSMC      _VAL2FLD(GPIO_AF,12)  /* FSMC Alternate Function mapping */
+#define GPIO_AF_FMC       _VAL2FLD(GPIO_AF,12)  /* FMC Alternate Function mapping */
+#define GPIO_AF_OTG_HS_FS _VAL2FLD(GPIO_AF,12)  /* OTG HS configured in FS, Alternate Function mapping */
+#define GPIO_AF_SDIO      _VAL2FLD(GPIO_AF,12)  /* SDIO Alternate Function mapping */
+#define GPIO_AF_DCMI      _VAL2FLD(GPIO_AF,13)  /* DCMI Alternate Function mapping */
+#define GPIO_AF_LTDC      _VAL2FLD(GPIO_AF,14)  /* LCD-TFT Alternate Function mapping */
+#define GPIO_AF_EVENTOUT  _VAL2FLD(GPIO_AF,15)  /* EVENTOUT Alternate Function mapping */
 
 #define GPIO_AF_OTG1_FS    GPIO_AF_OTG_FS
 #define GPIO_AF_OTG2_HS    GPIO_AF_OTG_HS
 #define GPIO_AF_OTG2_FS    GPIO_AF_OTG_HS_FS
 
-#define GPIO_Pin_0        (0x1u<< 0)
-#define GPIO_Pin_1        (0x1u<< 1)
-#define GPIO_Pin_2        (0x1u<< 2)
-#define GPIO_Pin_3        (0x1u<< 3)
-#define GPIO_Pin_4        (0x1u<< 4)
-#define GPIO_Pin_5        (0x1u<< 5)
-#define GPIO_Pin_6        (0x1u<< 6)
-#define GPIO_Pin_7        (0x1u<< 7)
-#define GPIO_Pin_8        (0x1u<< 8)
-#define GPIO_Pin_9        (0x1u<< 9)
-#define GPIO_Pin_10       (0x1u<<10)
-#define GPIO_Pin_11       (0x1u<<11)
-#define GPIO_Pin_12       (0x1u<<12)
-#define GPIO_Pin_13       (0x1u<<13)
-#define GPIO_Pin_14       (0x1u<<14)
-#define GPIO_Pin_15       (0x1u<<15)
-#define GPIO_Pin_All     ((0x1u<<16)-1)
+#define GPIO_Pin_0        (1UL<< 0U)
+#define GPIO_Pin_1        (1UL<< 1U)
+#define GPIO_Pin_2        (1UL<< 2U)
+#define GPIO_Pin_3        (1UL<< 3U)
+#define GPIO_Pin_4        (1UL<< 4U)
+#define GPIO_Pin_5        (1UL<< 5U)
+#define GPIO_Pin_6        (1UL<< 6U)
+#define GPIO_Pin_7        (1UL<< 7U)
+#define GPIO_Pin_8        (1UL<< 8U)
+#define GPIO_Pin_9        (1UL<< 9U)
+#define GPIO_Pin_10       (1UL<<10U)
+#define GPIO_Pin_11       (1UL<<11U)
+#define GPIO_Pin_12       (1UL<<12U)
+#define GPIO_Pin_13       (1UL<<13U)
+#define GPIO_Pin_14       (1UL<<14U)
+#define GPIO_Pin_15       (1UL<<15U)
+#define GPIO_Pin_All     ((1UL<<16U)-1)
 
 #define GPIO_Input_NoPull                (GPIO_Input | GPIO_NoPull)
 #define GPIO_Input_PullUp                (GPIO_Input | GPIO_PullUp)
@@ -179,7 +184,9 @@ struct __gpio_config
 
 /* -------------------------------------------------------------------------- */
 
-#define GPIO_PORT(gpio)  ((((uint32_t)(gpio))>>10)&0xF)
+#define GPIO_PORT_Pos     (10U)
+#define GPIO_PORT_Msk     (0xFUL << GPIO_PORT_Pos)
+#define GPIO_PORT(gpio)   _FLD2VAL(GPIO_PORT, gpio)
 
 /* -------------------------------------------------------------------------- */
 
