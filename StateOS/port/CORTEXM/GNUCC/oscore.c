ad48dce3c30b01e9edc17a35755102575e3f5a6c
Rajmund Szymanski <rajszym@o2.pl>
2017-11-01 20:01:04 +0100

    updated oscore for gcc compiler

--- StateOS/port/CORTEXM/GNUCC/oscore.c
+++ StateOS/port/CORTEXM/GNUCC/oscore.c
@@ -2,7 +2,7 @@
 
     @file    StateOS: oscore.c
     @author  Rajmund Szymanski
-    @date    31.10.2017
+    @date    01.11.2017
     @brief   StateOS port file for ARM Cotrex-M uC.
 
  ******************************************************************************
@@ -41,8 +41,8 @@ void PendSV_Handler( void )
 
 "	mrs   r0,    PSP               \n"
 "	mov   r3,    lr                \n"
-"	lsl   r3,  # 29                \n"
-"	bmi   priv_ctx_enter           \n"
+"	lsr   r3,  # 3                 \n"
+"	bcs   priv_ctx_enter           \n"
 "	mov   r0,    sp                \n"
 "	sub   sp,  # 36                \n"
 "priv_ctx_enter:                   \n"
@@ -55,6 +55,28 @@ void PendSV_Handler( void )
 "	mov   r7,    lr                \n"
 "	stm   r0!, { r3  - r7 }        \n"
 "	sub   r0,  # 36                \n"
+
+"	ldr   r1, =%[core_tsk_handler] \n"
+"	blx   r1                       \n"
+
+"	add   r0,  # 16                \n"
+"	ldm   r0!, { r3  - r7 }        \n"
+"	mov   r8,    r3                \n"
+"	mov   r9,    r4                \n"
+"	mov   r10,   r5                \n"
+"	mov   r11,   r6                \n"
+"	mov   lr,    r7                \n"
+"	sub   r0,  # 36                \n"
+"	ldm   r0!, { r4  - r7 }        \n"
+"	add   r0,  # 20                \n"
+"	mov   r3,    lr                \n"
+"	lsr   r3,  # 3                 \n"
+"	bcs   priv_ctx_exit            \n"
+"	mov   sp,    r0                \n"
+"	bx    lr                       \n"
+"priv_ctx_exit:                    \n"
+"	msr   PSP,   r0                \n"
+"	bx    lr                       \n"
 
 #else //__CORTEX_M
 
@@ -72,32 +94,7 @@ void PendSV_Handler( void )
 #endif
 "	stmdb r0!, { r4  - r11, lr }   \n"
 
-#endif//__CORTEX_M
-
 "	bl  %[core_tsk_handler]        \n"
-
-#if __CORTEX_M < 3
-
-"	add   r0,  # 16                \n"
-"	ldm   r0!, { r3  - r7 }        \n"
-"	mov   r8,    r3                \n"
-"	mov   r9,    r4                \n"
-"	mov   r10,   r5                \n"
-"	mov   r11,   r6                \n"
-"	mov   lr,    r7                \n"
-"	sub   r0,  # 36                \n"
-"	ldm   r0!, { r4  - r7 }        \n"
-"	add   r0,  # 20                \n"
-"	mov   r3,    lr                \n"
-"	lsl   r3,  # 29                \n"
-"	bmi   priv_ctx_exit            \n"
-"	mov   sp,    r0                \n"
-"	bx    lr                       \n"
-"priv_ctx_exit:                    \n"
-"	msr   PSP,   r0                \n"
-"	bx    lr                       \n"
-
-#else //__CORTEX_M
 
 "	ldmia r0!, { r4  - r11, lr }   \n"
 #if __FPU_USED
@@ -126,8 +123,18 @@ void core_tsk_flip( void *sp )
 	__asm volatile
 	(
 "	mov   sp,  %[sp]               \n"
+
+#if __CORTEX_M < 3
+
+"	ldr   r1, =%[core_tsk_loop]    \n"
+"	blx   r1                       \n"
+
+#else //__CORTEX_M
+
 "	bl  %[core_tsk_loop]           \n"
 	
+#endif//__CORTEX_M
+
 ::	[sp] "r" (sp),
 	[core_tsk_loop] "i" (core_tsk_loop)
 :	"memory"
