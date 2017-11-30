6116db44f1f180e27233f3e5269fa03ecb0e8448
rajszym <rajszym@o2.pl>
2017-11-30 06:42:28 +0100

    updated kernel files and examples

--- StateOS/kernel/src/os_sem.c
+++ StateOS/kernel/src/os_sem.c
@@ -2,7 +2,7 @@
 
     @file    StateOS: os_sem.c
     @author  Rajmund Szymanski
-    @date    22.10.2017
+    @date    30.11.2017
     @brief   This file provides set of functions for StateOS.
 
  ******************************************************************************
@@ -35,12 +35,13 @@ void sem_init( sem_t *sem, unsigned init, unsigned limit )
 	assert(!port_isr_inside());
 	assert(sem);
 	assert(limit);
+	assert(init<=limit);
 
 	port_sys_lock();
 
 	memset(sem, 0, sizeof(sem_t));
 
-	sem->count = UMIN(init, limit);
+	sem->count = init;
 	sem->limit = limit;
 
 	port_sys_unlock();
