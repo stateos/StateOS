ed11e82965160ed205724e2be97f6a770d56a051
rajszym <rajszym@o2.pl>
2018-04-23 08:19:45 +0200

    updated mailbox queue object

--- StateOS/kernel/src/os_box.c
+++ StateOS/kernel/src/os_box.c
@@ -2,7 +2,7 @@
 
     @file    StateOS: os_box.c
     @author  Rajmund Szymanski
-    @date    16.04.2018
+    @date    23.04.2018
     @brief   This file provides set of functions for StateOS.
 
  ******************************************************************************
@@ -115,7 +115,7 @@ void priv_box_get( box_t *box, char *data )
 	do data[i++] = box->data[f++]; while (i < box->size);
 
 	box->first = (f < box->limit) ? f : 0;
-	box->count--;
+	box->count -= i;
 }
 
 /* -------------------------------------------------------------------------- */
@@ -129,7 +129,7 @@ void priv_box_put( box_t *box, const char *data )
 	do box->data[n++] = data[i++]; while (i < box->size);
 
 	box->next = (n < box->limit) ? n : 0;
-	box->count++;
+	box->count += i;
 }
 
 /* -------------------------------------------------------------------------- */
