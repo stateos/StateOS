91bb814b35b274e37f895e45502c972940a8fbfa
Rajmund Szymański <rajszym@users.noreply.github.com>
2018-07-17 06:36:53 +0200

    updated spin lock object

--- StateOS/kernel/inc/osspinlock.h
+++ StateOS/kernel/inc/osspinlock.h
@@ -2,7 +2,7 @@
 
     @file    StateOS: osspinlock.h
     @author  Rajmund Szymanski
-    @date    16.07.2018
+    @date    17.07.2018
     @brief   This file contains definitions for StateOS.
 
  ******************************************************************************
@@ -264,11 +264,9 @@ void spn_init( spn_t *spn ) { *spn = 0; }
 
 struct SpinLock : private CriticalSection
 {
-	explicit
-	SpinLock( spn_id _spn ): spn(_spn) {}
-
-	void lock  ( void ) { core_spn_lock  (spn); }
-	void unlock( void ) { core_spn_unlock(spn); }
+	 explicit
+	 SpinLock( spn_id _spn ): spn(_spn) { core_spn_lock  (spn); }
+	~SpinLock( void )                   { core_spn_unlock(spn); }
 
 	private:
 	spn_id spn;
