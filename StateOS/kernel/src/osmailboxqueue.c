7553243f7d02a2e48ca00aa87669e9ab375f0964
Rajmund Szymański <rajszym@users.noreply.github.com>
2018-08-20 09:23:05 +0200

    updated mailbox queue object

--- StateOS/kernel/src/osmailboxqueue.c
+++ StateOS/kernel/src/osmailboxqueue.c
@@ -2,7 +2,7 @@
 
     @file    StateOS: osmailboxqueue.c
     @author  Rajmund Szymanski
-    @date    31.07.2018
+    @date    20.08.2018
     @brief   This file provides set of functions for StateOS.
 
  ******************************************************************************
@@ -123,16 +123,6 @@ unsigned priv_box_space( box_t *box )
 
 /* -------------------------------------------------------------------------- */
 static
-void priv_box_skip( box_t *box )
-/* -------------------------------------------------------------------------- */
-{
-	box->count -= box->size;
-	box->head  += box->size;
-	if (box->head == box->limit) box->head = 0;
-}
-
-/* -------------------------------------------------------------------------- */
-static
 void priv_box_get( box_t *box, char *data )
 /* -------------------------------------------------------------------------- */
 {
@@ -157,6 +147,16 @@ void priv_box_put( box_t *box, const char *data )
 
 	box->tail = (i < box->limit) ? i : 0;
 	box->count += j;
+}
+
+/* -------------------------------------------------------------------------- */
+static
+void priv_box_skip( box_t *box )
+/* -------------------------------------------------------------------------- */
+{
+	box->count -= box->size;
+	box->head  += box->size;
+	if (box->head == box->limit) box->head = 0;
 }
 
 /* -------------------------------------------------------------------------- */
