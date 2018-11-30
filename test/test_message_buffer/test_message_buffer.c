9a98bb9690150d78e5966e41e7de0304c6fceced
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-11-30 13:14:58 +0100

    updated message buffer test

--- test/test_message_buffer/test_message_buffer.c
+++ test/test_message_buffer/test_message_buffer.c
@@ -3,4 +3,8 @@
 void test_message_buffer()
 {
 	TEST_Notify();
+	TEST_Add(test_message_buffer_0);
+	TEST_Add(test_message_buffer_1);
+	TEST_Add(test_message_buffer_2);
+	TEST_Add(test_message_buffer_3);
 }
