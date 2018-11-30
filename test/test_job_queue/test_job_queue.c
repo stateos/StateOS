ecd88eea22f6fcce1152b1bf962356cdb9642c68
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-11-30 13:13:58 +0100

    updated job queue test

--- test/test_job_queue/test_job_queue.c
+++ test/test_job_queue/test_job_queue.c
@@ -3,4 +3,8 @@
 void test_job_queue()
 {
 	TEST_Notify();
+	TEST_Add(test_job_queue_0);
+	TEST_Add(test_job_queue_1);
+	TEST_Add(test_job_queue_2);
+	TEST_Add(test_job_queue_3);
 }
---
+++ test/test_job_queue/test_job_queue_3.c
@@ -0,0 +1,48 @@
+#include "test.h"
+
+static_JOB(job3, 1);
+
+static unsigned sent = 0;
+static unsigned received = 0;
+
+static void action()
+{
+	        received = sent;
+}
+
+static void proc1()
+{
+	unsigned event;
+
+	event = job_wait(job3);                      assert_success(event);
+	                                             assert(sent == received);
+	        tsk_stop();
+}
+
+static void proc2()
+{
+	unsigned event;
+
+	        sent = rand();
+	event = job_give(job3, action);              assert_success(event);
+	        tsk_stop();
+}
+
+static void test()
+{
+	unsigned event;
+		                                         assert_dead(tsk1);
+	        tsk_startFrom(tsk1, proc1);
+		                                         assert_dead(tsk2);
+	        tsk_startFrom(tsk2, proc2);
+	event = tsk_join(tsk2);                      assert_success(event);
+	event = tsk_join(tsk1);                      assert_success(event);
+}
+
+void test_job_queue_3()
+{
+	int i;
+	TEST_Notify();
+	for (i = 0; i < PASS; i++)
+		test();
+}
