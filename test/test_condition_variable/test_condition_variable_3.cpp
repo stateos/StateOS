915e599c2ce2f7fd5613b86392484166772805d9
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-10 13:37:20 +0100

    Create test_condition_variable_3.cpp

---
+++ test/test_condition_variable/test_condition_variable_3.cpp
@@ -0,0 +1,46 @@
+#include "test.h"
+
+static auto Mtx1 = Mutex(mtxDefault);
+static auto Cnd1 = ConditionVariable();
+
+static void proc1()
+{
+	unsigned event;
+
+	event = Mtx1.wait();                         assert_success(event);
+	        Cnd1.give(cndOne);
+	event = Mtx1.give();                         assert_success(event);
+	        ThisTask::stop();
+}
+
+static void proc2()
+{
+	unsigned event;
+	                                             assert(!Tsk1);
+	        Tsk1.startFrom(proc1);
+	event = Mtx1.wait();                         assert_success(event);
+	event = Cnd1.wait(Mtx1);                     assert_success(event);
+	        Cnd1.give(cndOne);
+	event = Mtx1.give();                         assert_success(event);
+	event = Tsk1.join();                         assert_success(event);
+	        ThisTask::stop();
+}
+
+static void test()
+{
+	unsigned event;
+
+	event = Mtx1.wait();                         assert_success(event);
+	                                             assert(!Tsk2);
+	        Tsk2.startFrom(proc2);
+	event = Cnd1.wait(Mtx1);                     assert_success(event);
+	event = Mtx1.give();                         assert_success(event);
+	event = Tsk2.join();                         assert_success(event);
+}
+
+extern "C"
+void test_condition_variable_3()
+{
+	TEST_Notify();
+	TEST_Call();
+}
