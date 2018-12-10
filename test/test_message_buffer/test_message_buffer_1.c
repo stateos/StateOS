d5f279e8ec50f23ac513c42f9e41b2a2c8c514c1
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-10 13:41:07 +0100

    Update test_message_buffer_1.c

--- test/test_message_buffer/test_message_buffer_1.c
+++ test/test_message_buffer/test_message_buffer_1.c
@@ -1,42 +1,78 @@
 #include "test.h"
 
-static unsigned sent = 0;
-static unsigned received = 0;
+#define SIZE sizeof(unsigned)
 
-static void proc1()
+static_MSG(msg3, 2 * SIZE);
+
+static unsigned sent;
+
+static void proc3()
 {
-	unsigned length;
+	unsigned bytes;
+	unsigned event;
 
- 	length = msg_wait(msg1, &received, sizeof(received));
- 	                                             assert(length == sizeof(received));
- 	                                             assert(sent == received);
+ 	bytes = msg_wait(msg3, &event, SIZE);        assert(bytes == SIZE);
+	                                             assert(event == sent);
+	event = msg_give(msg2, &event, SIZE);        assert_success(event);
 	        tsk_stop();
 }
 
 static void proc2()
 {
+	unsigned bytes;
 	unsigned event;
+		                                         assert_dead(tsk3);
+	        tsk_startFrom(tsk3, proc3);
+ 	bytes = msg_wait(msg2, &event, SIZE);        assert(bytes == SIZE);
+ 	                                             assert(event == sent);
+	event = msg_give(msg3, &event, SIZE);        assert_success(event);
+ 	bytes = msg_wait(msg2, &event, SIZE);        assert(bytes == SIZE);
+	                                             assert(event == sent);
+	event = msg_give(msg1, &event, SIZE);        assert_success(event);
+	event = tsk_join(tsk3);                      assert_success(event);
+	        tsk_stop();
+}
 
+static void proc1()
+{
+	unsigned bytes;
+	unsigned event;
+		                                         assert_dead(tsk2);
+	        tsk_startFrom(tsk2, proc2);
+ 	bytes = msg_wait(msg1, &event, SIZE);        assert(bytes == SIZE);
+ 	                                             assert(event == sent);
+	event = msg_give(msg2, &event, SIZE);        assert_success(event);
+ 	bytes = msg_wait(msg1, &event, SIZE);        assert(bytes == SIZE);
+ 	                                             assert(event == sent);
+	event = msg_give(&msg0, &event, SIZE);       assert_success(event);
+	event = tsk_join(tsk2);                      assert_success(event);
+	        tsk_stop();
+}
+
+static void proc0()
+{
+	unsigned bytes;
+	unsigned event;
+		                                         assert_dead(tsk1);
+	        tsk_startFrom(tsk1, proc1);
 	        sent = rand();
-	event = msg_give(msg1, &sent, sizeof(sent)); assert_success(event);
+	event = msg_give(msg1, &sent, SIZE);         assert_success(event);
+ 	bytes = msg_wait(&msg0, &event, SIZE);       assert(bytes == SIZE);
+ 	                                             assert(event == sent);
+	event = tsk_join(tsk1);                      assert_success(event);
 	        tsk_stop();
 }
 
 static void test()
 {
 	unsigned event;
-		                                         assert_dead(tsk1);
-	        tsk_startFrom(tsk1, proc1);
-		                                         assert_dead(tsk2);
-	        tsk_startFrom(tsk2, proc2);
-	event = tsk_join(tsk2);                      assert_success(event);
-	event = tsk_join(tsk1);                      assert_success(event);
+		                                         assert_dead(&tsk0);
+	        tsk_startFrom(&tsk0, proc0);
+	event = tsk_join(&tsk0);                     assert_success(event);
 }
 
 void test_message_buffer_1()
 {
-	int i;
 	TEST_Notify();
-	for (i = 0; i < PASS; i++)
-		test();
+	TEST_Call();
 }
