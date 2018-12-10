f99908e9949bef4735eb0f36f8b98c76a1d32edd
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-10 13:41:03 +0100

    Update test_message_buffer.c

--- test/test_message_buffer/test_message_buffer.c
+++ test/test_message_buffer/test_message_buffer.c
@@ -3,8 +3,8 @@
 void test_message_buffer()
 {
 	TEST_Notify();
-	TEST_Add(test_message_buffer_0);
 	TEST_Add(test_message_buffer_1);
+#ifndef __CSMC__
 	TEST_Add(test_message_buffer_2);
-	TEST_Add(test_message_buffer_3);
+#endif
 }
