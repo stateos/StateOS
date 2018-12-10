f18c060ab1180e9fec3ae5e638687bcfc9acde36
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-10 13:37:03 +0100

    Update test_condition_variable.c

--- test/test_condition_variable/test_condition_variable.c
+++ test/test_condition_variable/test_condition_variable.c
@@ -3,11 +3,9 @@
 void test_condition_variable()
 {
 	TEST_Notify();
-	TEST_Add(test_condition_variable_0);
 	TEST_Add(test_condition_variable_1);
+#ifndef __CSMC__
 	TEST_Add(test_condition_variable_2);
 	TEST_Add(test_condition_variable_3);
-#ifndef __CSMC__
-	TEST_Add(test_condition_variable_4);
 #endif
 }
