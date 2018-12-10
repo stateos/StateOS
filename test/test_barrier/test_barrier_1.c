16e1e221c6daa8b07a306f1da9141544c3b7d0c7
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-10 13:35:32 +0100

    Update test_barrier_1.c

--- test/test_barrier/test_barrier_1.c
+++ test/test_barrier/test_barrier_1.c
@@ -1,19 +1,47 @@
 #include "test.h"
 
-static void proc1()
+static_BAR(bar3, 4);
+
+static void proc3()
 {
 	unsigned event;
 
-	event = bar_wait(bar1);                      assert_success(event);
+	event = bar_wait(bar3);                      assert_success(event);
 	        tsk_stop();
 }
 
 static void proc2()
 {
 	unsigned event;
-	                                             assert_dead(tsk1);
-	        tsk_startFrom(tsk1, proc1);
+		                                         assert_dead(tsk3);
+	        tsk_startFrom(tsk3, proc3);
+	event = bar_wait(bar2);                      assert_success(event);
+	event = bar_wait(bar3);                      assert_success(event);
+	event = tsk_join(tsk3);                      assert_success(event);
+	        tsk_stop();
+}
+
+static void proc1()
+{
+	unsigned event;
+		                                         assert_dead(tsk2);
+	        tsk_startFrom(tsk2, proc2);
 	event = bar_wait(bar1);                      assert_success(event);
+	event = bar_wait(bar2);                      assert_success(event);
+	event = bar_wait(bar3);                      assert_success(event);
+	event = tsk_join(tsk2);                      assert_success(event);
+	        tsk_stop();
+}
+
+static void proc0()
+{
+	unsigned event;
+		                                         assert_dead(tsk1);
+	        tsk_startFrom(tsk1, proc1);
+	event = bar_wait(&bar0);                     assert_success(event);
+	event = bar_wait(bar1);                      assert_success(event);
+	event = bar_wait(bar2);                      assert_success(event);
+	event = bar_wait(bar3);                      assert_success(event);
 	event = tsk_join(tsk1);                      assert_success(event);
 	        tsk_stop();
 }
@@ -21,17 +49,16 @@ static void proc2()
 static void test()
 {
 	unsigned event;
-	                                             assert_dead(tsk2);
-	        tsk_startFrom(tsk2, proc2);
-	event = bar_wait(bar1);                      assert_success(event);
-	event = tsk_join(tsk2);                      assert_success(event);
+		                                         assert_dead(&tsk0);
+	        tsk_startFrom(&tsk0, proc0);
+	event = tsk_join(&tsk0);                     assert_success(event);
 }
 
 void test_barrier_1()
 {
-	int i;
 	TEST_Notify();
-	bar_init(bar1, 3);
-	for (i = 0; i < PASS; i++)
-		test();
+	bar_init(&bar0, 1);
+	bar_init(bar1, 2);
+	bar_init(bar2, 3);
+	TEST_Call();
 }
