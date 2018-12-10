b6ec3fadbcab16a54d324e39365f000a16042b5a
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-10 13:40:13 +0100

    Update test_mailbox_queue.c

--- test/test_mailbox_queue/test_mailbox_queue.c
+++ test/test_mailbox_queue/test_mailbox_queue.c
@@ -3,8 +3,8 @@
 void test_mailbox_queue()
 {
 	TEST_Notify();
-	TEST_Add(test_mailbox_queue_0);
 	TEST_Add(test_mailbox_queue_1);
+#ifndef __CSMC__
 	TEST_Add(test_mailbox_queue_2);
-	TEST_Add(test_mailbox_queue_3);
+#endif
 }
