d126f41f88ca23ac9547447a0c02ce5f014e464a
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-10 13:40:17 +0100

    Update test_mailbox_queue_1.c

--- test/test_mailbox_queue/test_mailbox_queue_1.c
+++ test/test_mailbox_queue/test_mailbox_queue_1.c
@@ -1,41 +1,76 @@
 #include "test.h"
 
-static unsigned sent = 0;
-static unsigned received = 0;
+static_BOX(box3, 1, sizeof(unsigned));
 
-static void proc1()
+static unsigned sent;
+
+static void proc3()
 {
+	unsigned received;
 	unsigned event;
 
- 	event = box_wait(box1, &received);           assert_success(event);
- 	                                             assert(sent == received);
+ 	event = box_wait(box3, &received);           assert_success(event);
+ 	                                             assert(received == sent);
+	event = box_give(box2, &received);           assert_success(event);
 	        tsk_stop();
 }
 
 static void proc2()
 {
+	unsigned received;
 	unsigned event;
+		                                         assert_dead(tsk3);
+	        tsk_startFrom(tsk3, proc3);
+ 	event = box_wait(box2, &received);           assert_success(event);
+ 	                                             assert(received == sent);
+	event = box_give(box3, &received);           assert_success(event);
+ 	event = box_wait(box2, &received);           assert_success(event);
+ 	                                             assert(received == sent);
+	event = box_give(box1, &received);           assert_success(event);
+	event = tsk_join(tsk3);                      assert_success(event);
+	        tsk_stop();
+}
 
+static void proc1()
+{
+	unsigned received;
+	unsigned event;
+		                                         assert_dead(tsk2);
+	        tsk_startFrom(tsk2, proc2);
+ 	event = box_wait(box1, &received);           assert_success(event);
+ 	                                             assert(received == sent);
+	event = box_give(box2, &received);           assert_success(event);
+ 	event = box_wait(box1, &received);           assert_success(event);
+ 	                                             assert(received == sent);
+	event = box_give(&box0, &received);          assert_success(event);
+	event = tsk_join(tsk2);                      assert_success(event);
+	        tsk_stop();
+}
+
+static void proc0()
+{
+	unsigned received;
+	unsigned event;
+		                                         assert_dead(tsk1);
+	        tsk_startFrom(tsk1, proc1);
 	        sent = rand();
 	event = box_give(box1, &sent);               assert_success(event);
+ 	event = box_wait(&box0, &received);          assert_success(event);
+ 	                                             assert(received == sent);
+	event = tsk_join(tsk1);                      assert_success(event);
 	        tsk_stop();
 }
 
 static void test()
 {
 	unsigned event;
-		                                         assert_dead(tsk1);
-	        tsk_startFrom(tsk1, proc1);
-		                                         assert_dead(tsk2);
-	        tsk_startFrom(tsk2, proc2);
-	event = tsk_join(tsk2);                      assert_success(event);
-	event = tsk_join(tsk1);                      assert_success(event);
+		                                         assert_dead(&tsk0);
+	        tsk_startFrom(&tsk0, proc0);
+	event = tsk_join(&tsk0);                     assert_success(event);
 }
 
 void test_mailbox_queue_1()
 {
-	int i;
 	TEST_Notify();
-	for (i = 0; i < PASS; i++)
-		test();
+	TEST_Call();
 }
