cfa3774cbd9a8b2f8730595e503ecdc6410a1864
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-11 07:51:34 +0100

    Update test_fast_mutex.c

--- test/test_fast_mutex/test_fast_mutex.c
+++ test/test_fast_mutex/test_fast_mutex.c
@@ -3,8 +3,8 @@
 void test_fast_mutex()
 {
 	TEST_Notify();
-	TEST_Add(test_fast_mutex_0);
 	TEST_Add(test_fast_mutex_1);
+#ifndef __CSMC__
 	TEST_Add(test_fast_mutex_2);
-	TEST_Add(test_fast_mutex_3);
+#endif
 }
