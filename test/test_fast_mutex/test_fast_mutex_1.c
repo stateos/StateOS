cb3aaeafab55aac978b8b7dafbc24807516dc9db
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-11 07:51:28 +0100

    Update test_fast_mutex_1.c

--- test/test_fast_mutex/test_fast_mutex_1.c
+++ test/test_fast_mutex/test_fast_mutex_1.c
@@ -1,32 +1,65 @@
 #include "test.h"
 
+static_MUT(mut3);
+
+static void proc3()
+{
+	unsigned event;
+
+	event = mut_take(mut2);                      assert_timeout(event);
+	event = mut_wait(mut3);                      assert_success(event);
+	event = mut_take(mut3);                      assert_failure(event);
+	event = mut_give(mut3);                      assert_success(event);
+	        tsk_stop();
+}
+
 static void proc2()
 {
 	unsigned event;
 
-	event = mut_wait(mut1);                      assert_success(event);
-	event = mut_give(mut1);                      assert_success(event);
-	                                             assert(!"test program cannot be caught here");
+	event = mut_take(mut1);                      assert_timeout(event);
+	event = mut_wait(mut2);                      assert_success(event);
+	event = mut_take(mut2);                      assert_failure(event);
+	                                             assert_dead(tsk3);
+	        tsk_startFrom(tsk3, proc3);
+	event = tsk_join(tsk3);                      assert_success(event);
+	event = mut_give(mut2);                      assert_success(event);
+	        tsk_stop();
 }
 
 static void proc1()
 {
 	unsigned event;
+
+	event = mut_take(&mut0);                     assert_timeout(event);
+	event = mut_wait(mut1);                      assert_success(event);
+	event = mut_take(mut1);                      assert_failure(event);
 	                                             assert_dead(tsk2);
 	        tsk_startFrom(tsk2, proc2);
-	event = tsk_kill(tsk2);                      assert_success(event);
+	event = tsk_join(tsk2);                      assert_success(event);
+	event = mut_give(mut1);                      assert_success(event);
+	        tsk_stop();
+}
+
+static void proc0()
+{
+	unsigned event;
+
+	event = mut_wait(&mut0);                     assert_success(event);
+	event = mut_take(&mut0);                     assert_failure(event);
+	                                             assert_dead(tsk1);
+	        tsk_startFrom(tsk1, proc1);
+	event = tsk_join(tsk1);                      assert_success(event);
+	event = mut_give(&mut0);                     assert_success(event);
 	        tsk_stop();
 }
 
 static void test()
 {
 	unsigned event;
-
-	event = mut_wait(mut1);                      assert_success(event);
-	                                             assert_dead(tsk1);
-	        tsk_startFrom(tsk1, proc1);
-	event = mut_give(mut1);                      assert_success(event);
-	event = tsk_join(tsk1);                      assert_success(event);
+	                                             assert_dead(&tsk0);
+	        tsk_startFrom(&tsk0, proc0);
+	event = tsk_join(&tsk0);                     assert_success(event);
 }
 
 void test_fast_mutex_1()
