f382f549ed747c66cbb830f98ebd320bed832ffa
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-11 12:16:09 +0100

    Create test_once_flag_2.cpp

---
+++ test/test_once_flag/test_once_flag_2.cpp
@@ -0,0 +1,65 @@
+#include "test.h"
+
+static_ONE(one3);
+
+static int counter;
+
+static void proc()
+{
+	        sys_lock();
+	        {
+		        counter++;
+	        }
+	        sys_unlock();
+}
+
+static void proc3()
+{
+	        one_call(one3, proc);
+	        tsk_stop();
+}
+
+static void proc2()
+{
+	unsigned event;
+		                                         assert_dead(tsk3);
+	        tsk_startFrom(tsk3, proc3);
+	        one_call(one2, proc);
+	event = tsk_join(tsk3);                      assert_success(event);
+	        tsk_stop();
+}
+
+static void proc1()
+{
+	unsigned event;
+		                                         assert_dead(tsk2);
+	        tsk_startFrom(tsk2, proc2);
+	        one_call(one1, proc);
+	event = tsk_join(tsk2);                      assert_success(event);
+	        tsk_stop();
+}
+
+static void proc0()
+{
+	unsigned event;
+		                                         assert_dead(tsk1);
+	        tsk_startFrom(tsk1, proc1);
+	        one_call(&one0, proc);               assert(counter == 1 || counter == 4);
+	event = tsk_join(tsk1);                      assert_success(event);
+	        tsk_stop();
+}
+
+static void test()
+{
+	unsigned event;
+		                                         assert_dead(&tsk0);
+	        tsk_startFrom(&tsk0, proc0);
+	event = tsk_join(&tsk0);                     assert_success(event);
+}
+
+extern "C"
+void test_once_flag_2()
+{
+	TEST_Notify();
+	TEST_Call();
+}
