9dfeebc1c03aa82a15f558fae2c962f60062a0e6
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-11 09:50:02 +0100

    Update test_alloc_2.c

--- test/test_alloc/test_alloc_2.c
+++ test/test_alloc/test_alloc_2.c
@@ -4,29 +4,53 @@ static sem_t *sem;
 
 static void proc3()
 {
-	tsk_stop();
+	unsigned event;
+
+	event = sem_wait(sem);                       assert_success(event);
+	        sem_delete(sem);
+	        tsk_stop();
 }
 
 static void proc2()
 {
 	tsk_t  * tsk;
 	unsigned event;
+
 	event = sem_wait(sem);                       assert_success(event);
-	sem_delete(sem);
+	        sem_delete(sem);
+	sem = sem_create(0, semBinary);              assert(sem);
 	tsk = tsk_create(3, proc3);                  assert(tsk);
+	event = sem_give(sem);                       assert_success(event);
 	event = tsk_join(tsk);                       assert_success(event);
-	tsk_stop();
+	        tsk_stop();
 }
 
 static void proc1()
 {
 	tsk_t  * tsk;
 	unsigned event;
+
+	event = sem_wait(sem);                       assert_success(event);
+	        sem_delete(sem);
 	sem = sem_create(0, semBinary);              assert(sem);
 	tsk = tsk_create(2, proc2);                  assert(tsk);
 	event = sem_give(sem);                       assert_success(event);
 	event = tsk_join(tsk);                       assert_success(event);
-	tsk_stop();
+	        tsk_stop();
+}
+
+static void proc0()
+{
+	tsk_t  * tsk;
+	unsigned event;
+
+	event = sem_wait(sem);                       assert_success(event);
+	        sem_delete(sem);
+	sem = sem_create(0, semBinary);              assert(sem);
+	tsk = tsk_create(1, proc1);                  assert(tsk);
+	event = sem_give(sem);                       assert_success(event);
+	event = tsk_join(tsk);                       assert_success(event);
+	        tsk_stop();
 }
 
 static void test()
@@ -34,7 +58,9 @@ static void test()
 	tsk_t  * tsk;
 	unsigned event;
 
-	tsk = tsk_create(1, proc1);                  assert(tsk);
+	sem   = sem_create(0, semBinary);            assert(sem);
+	tsk   = tsk_create(0, proc0);                assert(tsk);
+	event = sem_give(sem);                       assert_success(event);
 	event = tsk_join(tsk);                       assert_success(event);
 }
 
