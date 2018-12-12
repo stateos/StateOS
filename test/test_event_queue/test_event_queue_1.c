9844bd5814c8e19b922147c9f34eb06f08bdf723
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-12 13:52:13 +0100

    Update test_event_queue_1.c

--- test/test_event_queue/test_event_queue_1.c
+++ test/test_event_queue/test_event_queue_1.c
@@ -20,7 +20,7 @@ static void proc2()
 	unsigned received;
 	unsigned event;
 		                                         assert_dead(tsk3);
-	        tsk_startFrom(tsk3, proc3);
+	        tsk_startFrom(tsk3, proc3);          assert_ready(tsk3);
  	event = evq_wait(evq2, &received);           assert_success(event);
  	                                             assert(received == sent);
 	event = evq_give(evq3, received);            assert_success(event);
@@ -36,7 +36,7 @@ static void proc1()
 	unsigned received;
 	unsigned event;
 		                                         assert_dead(tsk2);
-	        tsk_startFrom(tsk2, proc2);
+	        tsk_startFrom(tsk2, proc2);          assert_ready(tsk2);
  	event = evq_wait(evq1, &received);           assert_success(event);
  	                                             assert(received == sent);
 	event = evq_give(evq2, received);            assert_success(event);
@@ -52,9 +52,10 @@ static void proc0()
 	unsigned received;
 	unsigned event;
 		                                         assert_dead(tsk1);
-	        tsk_startFrom(tsk1, proc1);
-	        sent = rand();
-	event = evq_give(evq1, sent);                assert_success(event);
+	        tsk_startFrom(tsk1, proc1);          assert_ready(tsk1);
+ 	event = evq_wait(&evq0, &received);          assert_success(event);
+ 	                                             assert(received == sent);
+	event = evq_give(evq1, received);            assert_success(event);
  	event = evq_wait(&evq0, &received);          assert_success(event);
  	                                             assert(received == sent);
 	event = tsk_join(tsk1);                      assert_success(event);
@@ -65,7 +66,8 @@ static void test()
 {
 	unsigned event;
 		                                         assert_dead(&tsk0);
-	        tsk_startFrom(&tsk0, proc0);
+	        tsk_startFrom(&tsk0, proc0);         assert_ready(&tsk0);
+	event = evq_give(&evq0, sent = rand());      assert_success(event);
 	event = tsk_join(&tsk0);                     assert_success(event);
 }
 
