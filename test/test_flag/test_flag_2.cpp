6e7a950b2e246e7206a2461feb8802bd33264efd
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-12 13:52:46 +0100

    Update test_flag_2.cpp

--- test/test_flag/test_flag_2.cpp
+++ test/test_flag/test_flag_2.cpp
@@ -1,12 +1,32 @@
 #include "test.h"
 
-static_FLG(flg3, ~0U);
+#define FLAG0 14U
+#define FLAG1 13U
+#define FLAG2 11U
+#define FLAG3  7U
+
+static_FLG(flg3, FLAG3);
+
+static void give(flg_t *flg, unsigned bitfield, unsigned check)
+{
+	unsigned flag;
+	unsigned flags;
+
+	while (bitfield)
+	{
+		flag = bitfield & (~bitfield + 1);
+		bitfield -= flag;
+		flags = flg_give(flg, flag);             assert(flags == check);
+	}
+}
 
 static void proc3()
 {
+	unsigned flags;
 	unsigned event;
 
-	event = flg_wait(flg3, 7, flgAll+flgProtect+flgIgnore); assert_success(event);
+	event = flg_wait(flg3, FLAG3, flgAll+flgProtect+flgIgnore); assert_success(event);
+	flags = flg_give(flg2, FLAG2);               assert(flags == FLAG2);
 	        tsk_stop();
 }
 
@@ -16,10 +36,10 @@ static void proc2()
 	unsigned event;
 	                                             assert_dead(tsk3);
 	        tsk_startFrom(tsk3, proc3);          assert_ready(tsk3);
-	event = flg_wait(flg2, 7, flgAll);           assert_success(event);
-	flags = flg_give(flg3, 2);                   assert(flags == ~0U);
-	flags = flg_give(flg3, 4);                   assert(flags == ~0U);
-	flags = flg_give(flg3, 1);                   assert(flags == ~0U);
+	event = flg_wait(flg2, FLAG2, flgAll);       assert_success(event);
+	        give(flg3, FLAG3, FLAG3);
+	event = flg_wait(flg2, FLAG2, flgAll);       assert_success(event);
+	flags = flg_give(flg1, FLAG1);               assert(flags == FLAG1);
 	event = tsk_join(tsk3);                      assert_success(event);
 	        tsk_stop();
 }
@@ -30,37 +50,34 @@ static void proc1()
 	unsigned event;
 	                                             assert_dead(tsk2);
 	        tsk_startFrom(tsk2, proc2);          assert_ready(tsk2);
-	event = flg_wait(flg1, 7, flgAll);           assert_success(event);
-	flags = flg_give(flg2, 1);                   assert(flags == 0);
-	flags = flg_give(flg2, 4);                   assert(flags == 0);
-	flags = flg_give(flg2, 2);                   assert(flags == 0);
+	event = flg_wait(flg1, FLAG1, flgAll);       assert_success(event);
+	        give(flg2, FLAG2, 0);
+	event = flg_wait(flg1, FLAG1, flgAll);       assert_success(event);
+	flags = flg_give(&flg0, FLAG0);              assert(flags == FLAG0);
 	event = tsk_join(tsk2);                      assert_success(event);
 	        tsk_stop();
 }
 
 static void proc0()
 {
-	unsigned flags;
 	unsigned event;
 	                                             assert_dead(tsk1);
 	        tsk_startFrom(tsk1, proc1);          assert_ready(tsk1);
-	event = flg_wait(&flg0, 7, flgAll);          assert_success(event);
-	flags = flg_give(flg1, 1);                   assert(flags == 0);
-	flags = flg_give(flg1, 4);                   assert(flags == 0);
-	flags = flg_give(flg1, 2);                   assert(flags == 0);
+	event = flg_wait(&flg0, FLAG0, flgAll);      assert_success(event);
+	        give(flg1, FLAG1, 0);
+	event = flg_wait(&flg0, FLAG0, flgAll);      assert_success(event);
 	event = tsk_join(tsk1);                      assert_success(event);
 	        tsk_stop();
 }
 
 static void test()
 {
-	unsigned flags;
 	unsigned event;
 	                                             assert_dead(&tsk0);
 	        tsk_startFrom(&tsk0, proc0);         assert_ready(&tsk0);
-	flags = flg_give(&flg0, 1);                  assert(flags == 0 || flags == 1);
-	flags = flg_give(&flg0, 4);                  assert(flags == 0 || flags == 4 || flags == 5);
-	flags = flg_give(&flg0, 2);                  assert(flags == 0 || flags == 2 || flags == 6 || flags == 7);
+	        tsk_yield();
+	        tsk_yield();
+	        give(&flg0, FLAG0, 0);
 	event = tsk_join(&tsk0);                     assert_success(event);
 }
 
