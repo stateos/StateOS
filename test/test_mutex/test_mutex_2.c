deb7308383a74e3c650c813d3adb9fde4e241ac6
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-13 13:44:31 +0100

    Update test_mutex_2.c

--- test/test_mutex/test_mutex_2.c
+++ test/test_mutex/test_mutex_2.c
@@ -1,37 +1,59 @@
 #include "test.h"
 
+static void proc4()
+{
+	unsigned event;
+
+	event = mtx_wait(mtx2);                      assert_success(event);
+	event = tsk_kill(tsk3);                      assert_success(event);
+	event = mtx_give(mtx2);                      assert_success(event);
+	        tsk_stop();
+}
+
+static void proc3()
+{
+	                                             assert_dead(tsk4);
+	        tsk_startFrom(tsk4, proc4);
+	                                             assert(!"test program cannot be caught here");
+}
+
 static void proc2()
 {
 	unsigned event;
 
 	event = mtx_wait(mtx2);                      assert_success(event);
+	event = mtx_wait(mtx1);                      assert_success(event);
+	event = tsk_kill(tsk1);                      assert_success(event);
+	event = mtx_give(mtx1);                      assert_success(event);
 	event = mtx_give(mtx2);                      assert_success(event);
-	                                             assert(!"test program cannot be caught here");
+	        tsk_stop();
 }
 
 static void proc1()
 {
-	unsigned event;
 	                                             assert_dead(tsk2);
 	        tsk_startFrom(tsk2, proc2);
-	event = tsk_kill(tsk2);                      assert_success(event);
-	        tsk_stop();
+	                                             assert(!"test program cannot be caught here");
 }
 
 static void test()
 {
 	unsigned event;
 
-	event = mtx_wait(mtx2);                      assert_success(event);
+	event = mtx_wait(mtx1);                      assert_success(event);
 	                                             assert_dead(tsk1);
 	        tsk_startFrom(tsk1, proc1);
-	event = mtx_give(mtx2);                      assert_success(event);
+	                                             assert_dead(tsk3);
+	        tsk_startFrom(tsk3, proc3);
+	event = mtx_give(mtx1);                      assert_success(event);
+	event = tsk_join(tsk3);                      assert_success(event);
 	event = tsk_join(tsk1);                      assert_success(event);
 }
 
 void test_mutex_2()
 {
 	TEST_Notify();
-	mtx_init(mtx2, mtxPrioNone, 0);
+	mtx_init(mtx1, mtxPrioInherit, 0);
+	mtx_init(mtx2, mtxPrioInherit, 0);
 	TEST_Call();
 }
