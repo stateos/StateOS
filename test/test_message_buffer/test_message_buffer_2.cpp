576625c8719d10ad16577939368856d4c6380b97
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-13 08:38:05 +0100

    Update test_message_buffer_2.cpp

--- test/test_message_buffer/test_message_buffer_2.cpp
+++ test/test_message_buffer/test_message_buffer_2.cpp
@@ -22,7 +22,7 @@ static void proc2()
 	unsigned bytes;
 	unsigned event;
 		                                         assert_dead(tsk3);
-	        tsk_startFrom(tsk3, proc3);
+	        tsk_startFrom(tsk3, proc3);          assert_ready(tsk3);
  	bytes = msg_wait(msg2, &event, SIZE);        assert(bytes == SIZE);
  	                                             assert(event == sent);
 	event = msg_give(msg3, &event, SIZE);        assert_success(event);
@@ -38,7 +38,7 @@ static void proc1()
 	unsigned bytes;
 	unsigned event;
 		                                         assert_dead(tsk2);
-	        tsk_startFrom(tsk2, proc2);
+	        tsk_startFrom(tsk2, proc2);          assert_ready(tsk2);
  	bytes = msg_wait(msg1, &event, SIZE);        assert(bytes == SIZE);
  	                                             assert(event == sent);
 	event = msg_give(msg2, &event, SIZE);        assert_success(event);
@@ -54,9 +54,10 @@ static void proc0()
 	unsigned bytes;
 	unsigned event;
 		                                         assert_dead(tsk1);
-	        tsk_startFrom(tsk1, proc1);
-	        sent = rand();
-	event = msg_give(msg1, &sent, SIZE);         assert_success(event);
+	        tsk_startFrom(tsk1, proc1);          assert_ready(tsk1);
+ 	bytes = msg_wait(&msg0, &event, SIZE);       assert(bytes == SIZE);
+ 	                                             assert(event == sent);
+	event = msg_give(msg1, &event, SIZE);        assert_success(event);
  	bytes = msg_wait(&msg0, &event, SIZE);       assert(bytes == SIZE);
  	                                             assert(event == sent);
 	event = tsk_join(tsk1);                      assert_success(event);
@@ -67,7 +68,11 @@ static void test()
 {
 	unsigned event;
 		                                         assert_dead(&tsk0);
-	        tsk_startFrom(&tsk0, proc0);
+	        tsk_startFrom(&tsk0, proc0);         assert_ready(&tsk0);
+	        tsk_yield();
+	        tsk_yield();
+	        sent = rand();
+	event = msg_give(&msg0, &sent, SIZE);        assert_success(event);
 	event = tsk_join(&tsk0);                     assert_success(event);
 }
 
