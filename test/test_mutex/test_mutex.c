1eb1779b8326f1f5c2e0727a217e4b99b8998b06
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-13 13:44:40 +0100

    Update test_mutex.c

--- test/test_mutex/test_mutex.c
+++ test/test_mutex/test_mutex.c
@@ -3,10 +3,10 @@
 void test_mutex()
 {
 	TEST_Notify();
-	TEST_Add(test_mutex_0);
 	TEST_Add(test_mutex_1);
 	TEST_Add(test_mutex_2);
 	TEST_Add(test_mutex_3);
+#ifndef __CSMC__
 	TEST_Add(test_mutex_4);
-	TEST_Add(test_mutex_5);
+#endif
 }
