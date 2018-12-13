fd84a9d0f59403abcaeee3b3528e43b607fb87e0
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-13 08:38:25 +0100

    Update test_semaphore_1.c

--- test/test_semaphore/test_semaphore_1.c
+++ test/test_semaphore/test_semaphore_1.c
@@ -15,7 +15,7 @@ static void proc2()
 {
 	unsigned event;
 		                                         assert_dead(tsk3);
-	        tsk_startFrom(tsk3, proc3);
+	        tsk_startFrom(tsk3, proc3);          assert_ready(tsk3);
  	event = sem_wait(sem2);                      assert_success(event);
  	event = sem_give(sem3);                      assert_success(event);
  	event = sem_wait(sem2);                      assert_success(event);
@@ -28,7 +28,7 @@ static void proc1()
 {
 	unsigned event;
 		                                         assert_dead(tsk2);
-	        tsk_startFrom(tsk2, proc2);
+	        tsk_startFrom(tsk2, proc2);          assert_ready(tsk2);
  	event = sem_wait(sem1);                      assert_success(event);
  	event = sem_give(sem2);                      assert_success(event);
  	event = sem_wait(sem1);                      assert_success(event);
@@ -41,7 +41,8 @@ static void proc0()
 {
 	unsigned event;
 		                                         assert_dead(tsk1);
-	        tsk_startFrom(tsk1, proc1);
+	        tsk_startFrom(tsk1, proc1);          assert_ready(tsk1);
+ 	event = sem_wait(&sem0);                     assert_success(event);
  	event = sem_give(sem1);                      assert_success(event);
  	event = sem_wait(&sem0);                     assert_success(event);
 	event = tsk_join(tsk1);                      assert_success(event);
@@ -52,7 +53,10 @@ static void test()
 {
 	unsigned event;
 		                                         assert_dead(&tsk0);
-	        tsk_startFrom(&tsk0, proc0);
+	        tsk_startFrom(&tsk0, proc0);         assert_ready(&tsk0);
+	        tsk_yield();
+	        tsk_yield();
+ 	event = sem_give(&sem0);                     assert_success(event);
 	event = tsk_join(&tsk0);                     assert_success(event);
 }
 
