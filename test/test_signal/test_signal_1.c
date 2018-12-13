502719ab15da9450989d6a81e01074c5b5747207
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-13 08:38:36 +0100

    Update test_signal_1.c

--- test/test_signal/test_signal_1.c
+++ test/test_signal/test_signal_1.c
@@ -17,7 +17,7 @@ static void proc2()
 {
 	unsigned event;
 		                                         assert_dead(tsk3);
-	        tsk_startFrom(tsk3, proc3);
+	        tsk_startFrom(tsk3, proc3);          assert_ready(tsk3);
 	event = sig_wait(sig2, sigAll);              assert(event == sent);
  	        sig_give(sig3, event);
 	event = sig_wait(sig2, sigAll);              assert(event == sent);
@@ -30,7 +30,7 @@ static void proc1()
 {
 	unsigned event;
 		                                         assert_dead(tsk2);
-	        tsk_startFrom(tsk2, proc2);
+	        tsk_startFrom(tsk2, proc2);          assert_ready(tsk2);
 	event = sig_wait(sig1, sigAll);              assert(event == sent);
  	        sig_give(sig2, event);
 	event = sig_wait(sig1, sigAll);              assert(event == sent);
@@ -43,8 +43,8 @@ static void proc0()
 {
 	unsigned event;
 		                                         assert_dead(tsk1);
-	        tsk_startFrom(tsk1, proc1);
-	        sent = rand() % SIG_LIMIT;
+	        tsk_startFrom(tsk1, proc1);          assert_ready(tsk1);
+	event = sig_wait(&sig0, sigAll);             assert(event == sent);
  	        sig_give(sig1, sent);
 	event = sig_wait(&sig0, sigAll);             assert(event == sent);
 	event = tsk_join(tsk1);                      assert_success(event);
@@ -55,7 +55,10 @@ static void test()
 {
 	unsigned event;
 		                                         assert_dead(&tsk0);
-	        tsk_startFrom(&tsk0, proc0);
+	        tsk_startFrom(&tsk0, proc0);         assert_ready(&tsk0);
+	        tsk_yield();
+	        tsk_yield();
+ 	        sig_give(&sig0, sent = rand() % SIG_LIMIT);
 	event = tsk_join(&tsk0);                     assert_success(event);
 }
 
