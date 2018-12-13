976ab1ab8c7f34541151c900046792c21bccc315
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-13 06:40:12 +0100

    Update test_mailbox_queue_1.c

--- test/test_mailbox_queue/test_mailbox_queue_1.c
+++ test/test_mailbox_queue/test_mailbox_queue_1.c
@@ -20,7 +20,7 @@ static void proc2()
 	unsigned received;
 	unsigned event;
 		                                         assert_dead(tsk3);
-	        tsk_startFrom(tsk3, proc3);
+	        tsk_startFrom(tsk3, proc3);          assert_ready(tsk3);
  	event = box_wait(box2, &received);           assert_success(event);
  	                                             assert(received == sent);
 	event = box_give(box3, &received);           assert_success(event);
@@ -36,7 +36,7 @@ static void proc1()
 	unsigned received;
 	unsigned event;
 		                                         assert_dead(tsk2);
-	        tsk_startFrom(tsk2, proc2);
+	        tsk_startFrom(tsk2, proc2);          assert_ready(tsk2);
  	event = box_wait(box1, &received);           assert_success(event);
  	                                             assert(received == sent);
 	event = box_give(box2, &received);           assert_success(event);
@@ -52,9 +52,10 @@ static void proc0()
 	unsigned received;
 	unsigned event;
 		                                         assert_dead(tsk1);
-	        tsk_startFrom(tsk1, proc1);
-	        sent = rand();
-	event = box_give(box1, &sent);               assert_success(event);
+	        tsk_startFrom(tsk1, proc1);          assert_ready(tsk1);
+ 	event = box_wait(&box0, &received);          assert_success(event);
+ 	                                             assert(received == sent);
+	event = box_give(box1, &received);           assert_success(event);
  	event = box_wait(&box0, &received);          assert_success(event);
  	                                             assert(received == sent);
 	event = tsk_join(tsk1);                      assert_success(event);
@@ -65,7 +66,11 @@ static void test()
 {
 	unsigned event;
 		                                         assert_dead(&tsk0);
-	        tsk_startFrom(&tsk0, proc0);
+	        tsk_startFrom(&tsk0, proc0);         assert_ready(&tsk0);
+	        tsk_yield();
+	        tsk_yield();
+	        sent = rand();
+	event = box_give(&box0, &sent);              assert_success(event);
 	event = tsk_join(&tsk0);                     assert_success(event);
 }
 
