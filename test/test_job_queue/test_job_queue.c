faf9c3460ff32faee046630f16019e584e17214f
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-13 06:39:52 +0100

    Update test_job_queue.c

--- test/test_job_queue/test_job_queue.c
+++ test/test_job_queue/test_job_queue.c
@@ -3,8 +3,9 @@
 void test_job_queue()
 {
 	TEST_Notify();
-	TEST_Add(test_job_queue_0);
 	TEST_Add(test_job_queue_1);
+#ifndef __CSMC__
 	TEST_Add(test_job_queue_2);
 	TEST_Add(test_job_queue_3);
+#endif
 }
