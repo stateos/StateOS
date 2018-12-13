5490e2af0c69030e82642e0d0b4793f9cc06952d
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-13 08:37:55 +0100

    Update test_memory_pool.c

--- test/test_memory_pool/test_memory_pool.c
+++ test/test_memory_pool/test_memory_pool.c
@@ -3,8 +3,9 @@
 void test_memory_pool()
 {
 	TEST_Notify();
-	TEST_Add(test_memory_pool_0);
 	TEST_Add(test_memory_pool_1);
+#ifndef __CSMC__
 	TEST_Add(test_memory_pool_2);
 	TEST_Add(test_memory_pool_3);
+#endif
 }
