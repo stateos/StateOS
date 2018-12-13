81db6827a1c719bda374a2ccbb2692f75a1efcbb
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-13 08:36:33 +0100

    Update test_barrier_3.cpp

--- test/test_barrier/test_barrier_3.cpp
+++ test/test_barrier/test_barrier_3.cpp
@@ -1,15 +1,16 @@
 #include "test.h"
 
 auto Bar0 = Barrier(2);
-auto Bar1 = Barrier(3);
-auto Bar2 = Barrier(4);
-auto Bar3 = Barrier(5);
+auto Bar1 = Barrier(2);
+auto Bar2 = Barrier(2);
+auto Bar3 = Barrier(2);
 
 static void proc3()
 {
 	unsigned event;
 
 	event = Bar3.wait();                         assert_success(event);
+	event = Bar2.wait();                         assert_success(event);
 	        ThisTask::stop();
 }
 
@@ -20,6 +21,8 @@ static void proc2()
 	        Tsk3.startFrom(proc3);               assert(!!Tsk3);
 	event = Bar2.wait();                         assert_success(event);
 	event = Bar3.wait();                         assert_success(event);
+	event = Bar2.wait();                         assert_success(event);
+	event = Bar1.wait();                         assert_success(event);
 	event = Tsk3.join();                         assert_success(event);
 	        ThisTask::stop();
 }
@@ -31,7 +34,8 @@ static void proc1()
 	        Tsk2.startFrom(proc2);               assert(!!Tsk2);
 	event = Bar1.wait();                         assert_success(event);
 	event = Bar2.wait();                         assert_success(event);
-	event = Bar3.wait();                         assert_success(event);
+	event = Bar1.wait();                         assert_success(event);
+	event = Bar0.wait();                         assert_success(event);
 	event = Tsk2.join();                         assert_success(event);
 	        ThisTask::stop();
 }
@@ -43,8 +47,7 @@ static void proc0()
 	        Tsk1.startFrom(proc1);               assert(!!Tsk1);
 	event = Bar0.wait();                         assert_success(event);
 	event = Bar1.wait();                         assert_success(event);
-	event = Bar2.wait();                         assert_success(event);
-	event = Bar3.wait();                         assert_success(event);
+	event = Bar0.wait();                         assert_success(event);
 	event = Tsk1.join();                         assert_success(event);
 	        ThisTask::stop();
 }
@@ -55,9 +58,6 @@ static void test()
 		                                         assert(!Tsk0);
 	        Tsk0.startFrom(proc0);               assert(!!Tsk0);
 	event = Bar0.wait();                         assert_success(event);
-	event = Bar1.wait();                         assert_success(event);
-	event = Bar2.wait();                         assert_success(event);
-	event = Bar3.wait();                         assert_success(event);
 	event = Tsk0.join();                         assert_success(event);
 }
 
