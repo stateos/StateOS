cc2f57ff6e250718eb530ec36ba72f689d55ab52
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-17 06:21:56 +0100

    Update test_task.c

--- test/test_task/test_task.c
+++ test/test_task/test_task.c
@@ -10,6 +10,10 @@ void test_task()
 	TEST_Add(test_task_infinite_loop_1);
 	TEST_Add(test_task_signal_1);
 #ifndef __CSMC__
+	TEST_Add(test_task_infinite_loop_2);
+	TEST_Add(test_task_infinite_loop_3);
+	TEST_Add(test_task_signal_2);
+	TEST_Add(test_task_signal_3);
 	TEST_Add(test_task_create_4);
 	TEST_Add(test_task_create_5);
 #endif
