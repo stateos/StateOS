45abb5d902cec182622ee6bc9b63fc02dd9183c2
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-17 06:21:01 +0100

    Update test_task_signal_1.c

--- test/test_task/test_task_signal_1.c
+++ test/test_task/test_task_signal_1.c
@@ -8,6 +8,12 @@ static void action( unsigned signo )
 	        received = signo;
 }
 
+static void proc2()
+{
+	        tsk_signal(tsk1, sent = rand() % SIG_LIMIT);
+	        tsk_stop();
+}
+
 static void proc1()
 {
 	        cur_action(action);
@@ -16,20 +22,13 @@ static void proc1()
 	        tsk_stop();
 }
 
-static void proc2()
-{
-	        sent = rand() % SIG_LIMIT;
-	        tsk_signal(tsk1, sent);
-	        tsk_stop();
-}
-
 static void test()
 {
 	unsigned event;
 		                                         assert_dead(tsk1);
-	        tsk_startFrom(tsk1, proc1);
+	        tsk_startFrom(tsk1, proc1);          assert_ready(tsk1);
 		                                         assert_dead(tsk2);
-	        tsk_startFrom(tsk2, proc2);
+	        tsk_startFrom(tsk2, proc2);          assert_dead(tsk2);
 	event = tsk_join(tsk2);                      assert_success(event);
 	event = tsk_resume(tsk1);                    assert_success(event);
 	event = tsk_join(tsk1);                      assert_success(event);
