6718d3bd52bb9010aeb4d12cc0e6e46ef7ef8d76
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-19 09:27:49 +0100

    Update test_task_create_1.c

--- test/test_task/test_task_create_1.c
+++ test/test_task/test_task_create_1.c
@@ -9,19 +9,19 @@ static void test()
 {
 	unsigned event;
 
-	tsk_t *tsk6 = TSK_CREATE(6, proc, 256);
-	tsk_id tsk7 = TSK_CREATE(7, proc);
-	tsk_t *tsk8 = TSK_CREATE(8, proc, 512);
 	tsk_id tsk9 = TSK_CREATE(9, proc);
+	tsk_t *tsk8 = TSK_CREATE(8, proc, 512);
+	tsk_id tsk7 = TSK_CREATE(7, proc);
+	tsk_t *tsk6 = TSK_CREATE(6, proc, 256);
 
-	        tsk_start(tsk6);
-	event = tsk_join(tsk6);                      assert_success(event);
-	        tsk_start(tsk7);
-	event = tsk_join(tsk7);                      assert_success(event);
-	        tsk_start(tsk8);
-	event = tsk_join(tsk8);                      assert_success(event);
-	        tsk_start(tsk9);
+	        tsk_start(tsk9);                     assert_dead(tsk9);
 	event = tsk_join(tsk9);                      assert_success(event);
+	        tsk_start(tsk8);                     assert_dead(tsk8);
+	event = tsk_join(tsk8);                      assert_success(event);
+	        tsk_start(tsk7);                     assert_dead(tsk7);
+	event = tsk_join(tsk7);                      assert_success(event);
+	        tsk_start(tsk6);                     assert_dead(tsk6);
+	event = tsk_join(tsk6);                      assert_success(event);
 }
 
 void test_task_create_1()
