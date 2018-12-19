e7c6040b7dc35e2ebeb896e33505fb4b3f593878
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-19 09:28:13 +0100

    Create test_timer_2.cpp

---
+++ test/test_timer/test_timer_2.cpp
@@ -0,0 +1,37 @@
+#include "test.h"
+
+static_TMR_DEF(tmr3) {}
+
+static int counter;
+
+static void proc()
+{
+	        sys_lock();
+	        {
+		        counter++;
+	        }
+	        sys_unlock();
+}
+
+static void test()
+{
+	unsigned event;
+
+	        counter = 0;
+	        tmr_startFrom(tmr3, 3, 0, proc);
+	        tmr_startFrom(tmr2, 2, 0, proc);
+	        tmr_startFrom(tmr1, 1, 0, proc);
+	        tmr_startFrom(&tmr0, 0, 0, proc);
+	event = tmr_wait(&tmr0);                     assert_success(event);
+	event = tmr_wait(tmr1);                      assert_success(event);
+	event = tmr_wait(tmr2);                      assert_success(event);
+	event = tmr_wait(tmr3);                      assert_success(event);
+	                                             assert(counter == 4);
+}
+
+extern "C"
+void test_timer_2()
+{
+	TEST_Notify();
+	TEST_Call();
+}
