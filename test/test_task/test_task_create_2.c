a998711bd7789d382ccf890af865912677063f95
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-19 09:27:54 +0100

    Update test_task_create_2.c

--- test/test_task/test_task_create_2.c
+++ test/test_task/test_task_create_2.c
@@ -1,25 +1,43 @@
 #include "test.h"
 
-static_BAR(bar3, 5);
+static_TSK_START(tsk9, 9)
+{
+	        tsk_stop();
+}
 
-static_TSK_START(tsk6, 6)      { bar_wait(bar3); tsk_stop(); }
-static_TSK_START(tsk7, 7, 512) { bar_wait(bar3); tsk_stop(); }
-static_TSK_START(tsk8, 8, 256) { bar_wait(bar3); tsk_stop(); }
-static_TSK_START(tsk9, 9)      { bar_wait(bar3); tsk_stop(); }
+static_TSK_START(tsk8, 8, 256)
+{
+	unsigned event;
+
+	        tsk_start(tsk9);                     assert_dead(tsk9);
+	event = tsk_join(tsk9);                      assert_success(event);
+	        tsk_stop();
+}
+
+static_TSK_START(tsk7, 7, 512)
+{
+	unsigned event;
+
+	        tsk_start(tsk8);                     assert_dead(tsk8);
+	event = tsk_join(tsk8);                      assert_success(event);
+	        tsk_stop();
+}
+
+static_TSK_START(tsk6, 6)
+{
+	unsigned event;
+
+	        tsk_start(tsk7);                     assert_dead(tsk7);
+	event = tsk_join(tsk7);                      assert_success(event);
+	        tsk_stop();
+}
 
 static void test()
 {
 	unsigned event;
 
-	event = bar_wait(bar3);                      assert_success(event);
-	                                             assert_dead(tsk6);
-	        tsk_start(tsk6);
-	                                             assert_dead(tsk7);
-	        tsk_start(tsk7);
-	                                             assert_dead(tsk8);
-	        tsk_start(tsk8);
-	                                             assert_dead(tsk9);
-	        tsk_start(tsk9);
+	        tsk_start(tsk6);                     assert_dead(tsk6);
+	event = tsk_join(tsk6);                      assert_success(event);
 }
 
 void test_task_create_2()
