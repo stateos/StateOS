33c0f733384ab2f26fec7d10c145647ebab20791
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-19 09:27:59 +0100

    Update test_task_create_3.c

--- test/test_task/test_task_create_3.c
+++ test/test_task/test_task_create_3.c
@@ -1,31 +1,47 @@
 #include "test.h"
 
-static void proc()
+static void proc9()
 {
+	        tsk_stop();
+}
+
+static void proc8()
+{
+	unsigned event;
+	tsk_t *tsk9 = tsk_create(9, proc9);
+
+	        tsk_start(tsk9);                     assert_dead(tsk9);
+	event = tsk_join(tsk9);                      assert_success(event);
+	        tsk_stop();
+}
+
+static void proc7()
+{
+	unsigned event;
+	tsk_t *tsk8 = wrk_create(8, proc8, 512);
+
+	        tsk_start(tsk8);                     assert_dead(tsk8);
+	event = tsk_join(tsk8);                      assert_success(event);
+	        tsk_stop();
+}
+
+static void proc6()
+{
+	unsigned event;
+	tsk_t *tsk7 = wrk_create(7, proc7, 256);
+
+	        tsk_start(tsk7);                     assert_dead(tsk7);
+	event = tsk_join(tsk7);                      assert_success(event);
 	        tsk_stop();
 }
 
 static void test()
 {
 	unsigned event;
+	tsk_t *tsk6 = tsk_create(6, proc6);
 
-	tsk_t *tsk6 = tsk_create(6, proc);
-	tsk_t *tsk7 = wrk_create(7, proc, 256);
-	tsk_t *tsk8 = wrk_create(8, proc, 512);
-	tsk_t *tsk9 = tsk_create(9, proc);
-
-	        tsk_start(tsk6);
+	        tsk_start(tsk6);                     assert_dead(tsk6);
 	event = tsk_join(tsk6);                      assert_success(event);
-	                                             assert(tsk6->hdr.obj.res == RELEASED);
-	        tsk_start(tsk7);
-	event = tsk_join(tsk7);                      assert_success(event);
-	                                             assert(tsk7->hdr.obj.res == RELEASED);
-	        tsk_start(tsk8);
-	event = tsk_join(tsk8);                      assert_success(event);
-	                                             assert(tsk8->hdr.obj.res == RELEASED);
-	        tsk_start(tsk9);
-	event = tsk_join(tsk9);                      assert_success(event);
-	                                             assert(tsk9->hdr.obj.res == RELEASED);
 }
 
 void test_task_create_3()
