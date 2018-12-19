36df024072dd512f89fe5a3b532393e64344d6fd
Szymański Rajmund <szymanski.rajmund@zepak.net>
2018-12-19 11:23:01 +0100

    Update test_timer_2.cpp

--- test/test_timer/test_timer_2.cpp
+++ test/test_timer/test_timer_2.cpp
@@ -2,6 +2,11 @@
 
 static_TMR_DEF(tmr3) {}
 
+static_TMR_START(tmr4, 4, 0) {}
+static_TMR_START(tmr5, 5, 0) {}
+static_TMR_START(tmr6, 6, 0) {}
+static_TMR_START(tmr7, 7, 0) {}
+
 static int counter;
 
 static void proc()
@@ -18,6 +23,10 @@ static void test()
 	unsigned event;
 
 	        counter = 0;
+	event = tmr_wait(tmr4);                      assert_success(event);
+	event = tmr_wait(tmr5);                      assert_success(event);
+	event = tmr_wait(tmr6);                      assert_success(event);
+	event = tmr_wait(tmr7);                      assert_success(event);
 	        tmr_startFrom(tmr3, 3, 0, proc);
 	        tmr_startFrom(tmr2, 2, 0, proc);
 	        tmr_startFrom(tmr1, 1, 0, proc);
