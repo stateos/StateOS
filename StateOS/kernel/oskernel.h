d141573d0a076f6bf4e95c4bfd173e40bacc9800
rajszym <rajszym@o2.pl>
2020-05-20 21:10:37 +0200

    updated stack assertion

--- StateOS/kernel/oskernel.h
+++ StateOS/kernel/oskernel.h
@@ -95,7 +95,7 @@ extern sys_t System; // system data
         assert(((tsk) == &MAIN) || ((uintptr_t)(tsk)->stack + (OS_GUARD_SIZE) < (uintptr_t)(sp)))
 
 #define assert_stk_integrity() \
-        assert((System.cur == &MAIN) || (core_stk_space() > (OS_GUARD_SIZE)))
+        assert((System.cur == &MAIN) || (System.cur == &IDLE) || (core_stk_space() > (OS_GUARD_SIZE)))
 
 #define assert_tsk_context() \
         assert(port_isr_context() == false)
