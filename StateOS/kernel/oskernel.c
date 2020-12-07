b90ed1e093e375a90f26ab67f8e69951e479f616
rajszym <rajszym@o2.pl>
2020-12-07 15:39:17 +0100

    preventing sign-conversion warnings

--- StateOS/kernel/oskernel.c
+++ StateOS/kernel/oskernel.c
@@ -2,7 +2,7 @@
 
     @file    StateOS: oskernel.c
     @author  Rajmund Szymanski
-    @date    25.06.2020
+    @date    07.12.2020
     @brief   This file provides set of variables and functions for StateOS.
 
  ******************************************************************************
@@ -280,7 +280,7 @@ void core_ctx_init( tsk_t *tsk )
 size_t core_stk_space( tsk_t *tsk )
 {
 	void *stk = tsk->stack;
-	char *ptr = stk;
+	uint8_t *ptr = stk;
 	while (*ptr == 0xFF) ptr++;
 	return (uintptr_t)ptr - (uintptr_t)stk;
 }
