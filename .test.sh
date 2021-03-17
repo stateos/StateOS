adfa1328424ec1319decbf981a6b79095eed543f
Szymański Rajmund <rajszym@o2.pl>
2021-03-17 13:32:45 +0100

    updated test files

--- .test.sh
+++ .test.sh
@@ -2,23 +2,20 @@
 
 set -e
 
-if [ ! -d examples ]; then
-	make all -f makefile.gnucc
-	exit 0
-fi
-
 for file in examples/*.c
 do
-	rm -f examples/main.*
 	echo $file
 	cp $file examples/main.c
-	make all -f c.make
+	make -f examples.make all
+	rm -f examples/main.*
 done
 
 for file in examples/*.cpp
 do
-	rm -f examples/main.*
 	echo $file
 	cp $file examples/main.cpp
-	make all -f cpp.make
+	make -f examples.make all
+	rm -f examples/main.*
 done
+
+make -f examples.make clean
