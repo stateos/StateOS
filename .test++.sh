06dfe0324fac23287443cd987a9e54994c499135
Szymański Rajmund <szymanski.rajmund@zepak.net>
2021-04-26 11:23:19 +0200

    Revert "updated test files"
    This reverts commit 372bdaafbce20992f93b7932d8217b33f376f55f.

--- .test++.sh
+++ .test++.sh
@@ -6,8 +6,8 @@ for file in stdc++/*.cpp
 do
 	echo $file
 	cp $file stdc++/main.cpp
-	touch stdc++/main.cpp
 	make -f stdc++.make all
+	rm -f stdc++/main.*
 done
 
 make -f stdc++.make clean
--- .test.sh
+++ .test.sh
@@ -6,16 +6,16 @@ for file in examples/*.c
 do
 	echo $file
 	cp $file examples/main.c
-	touch examples/main.c
 	make -f examples.make all
+	rm -f examples/main.*
 done
 
 for file in examples/*.cpp
 do
 	echo $file
 	cp $file examples/main.cpp
-	touch examples/main.cpp
 	make -f examples.make all
+	rm -f examples/main.*
 done
 
 make -f examples.make clean
